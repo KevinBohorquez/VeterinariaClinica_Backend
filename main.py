5c4807d7e89f7c38f6ad53e60ca975e7e518bfc6
SubDSR <david.sevan@unmsm.edu.pe>
2025-06-09 23:19:53 -0500

    Agregando CORS para conexion con el Front

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ app.add_middleware(
     allow_origins=[
         "*",
         "http://localhost:5173",
+        "http://localhost:3000",
         "https://colitasfelices.netlify.app/"  # Frontend local       
                    ],
     allow_credentials=True,
